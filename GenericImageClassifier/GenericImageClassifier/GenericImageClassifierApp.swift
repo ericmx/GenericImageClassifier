c07f4a2d053b7de56d0d7174b15249c21e096c50
Eric May <eric.may@boldpenguin.com>
2023-09-12 10:34:26 -0400

    init

---
+++ GenericImageClassifier/GenericImageClassifier/GenericImageClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericImageClassifierApp.swift
+//  GenericImageClassifier
+//
+//  Created by Eric May on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GenericImageClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
